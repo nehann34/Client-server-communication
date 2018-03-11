a6fcfe6a2ab0319cff96361e76831519287ca4d9
Neha Nagpal <nehanagpal.nn34@gmail.com>
2018-03-11 22:36:24 +0530

    Communication bw 2 and more clients

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-
+import threading
 def rec_whole_size(sock,size):
     r=0
     array= bytes()
@@ -8,33 +8,30 @@ def rec_whole_size(sock,size):
           r=len(array)
     return array
 
-def length_to_three(size)
+def length_to_three(size):
     size='0'*(3-len(size))+ size
     return size
 
 #receive messages from server
 def rec_msg_fromserver(server_sock):
-    msg_size= int(recv_whole_size(server_sock, 3).decode("utf-8"))
+    msg_size= int(rec_whole_size(server_sock, 3).decode("utf-8"))
     message = rec_whole_size(server_sock,msg_size).decode("utf-8")
-    print(message)
+    print(message)    
+
 
 print("Hi,I'm client")
-user= input("Please enter username:")
+username= input("Username:")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("socket created")
-
 print("connecting to server.....")
-s.connect(('192.168.43.50',6565))
+s.connect(('192.168.43.50',6333))
 print("Now you are connected to server,Start the communication.")
-
 
 t = threading.Thread(target = rec_msg_fromserver, args=[s])
 t.start()
 
 while True :
-      message = username + ":" + input()
-      size = toThree( str(len(message)) )
-      message = (size + message).encode('utf-8')
+      message =username + ' sent : '+input()
+      size = length_to_three(str(len(message)) )
+      message = (size + message).encode("utf-8")
       s.sendall(message)
-
-
--- server.py
+++ server.py
@@ -1,9 +1,55 @@
 import socket
+import threading
+import queue
 
+q_list=[]
+q_list_lock=threading.Lock()
+
+def rec_whole_size(sock, size):
+	r = 0
+	array = bytes()
+
+	while r < size:
+		array = array + sock.recv(size - r)
+		r = len(array)
+	return array
+
+def send_to_client(my_q,client_sock):
+    while True:
+           
+          message=my_q.get()
+         # print("sending to client")
+          client_sock.sendall(message)
+          
+
+def rec_from_client(my_q,client_sock):
+    while True:
+  
+         message_size = rec_whole_size(client_sock, 3)
+         int_message_size = int(message_size.decode('utf-8'))
+         message = rec_whole_size(client_sock, int_message_size)
+         print("Client",message.decode('utf-8'))
+         with q_list_lock:
+              for q in q_list:
+                  if q is my_q:
+                      pass
+                  else :
+                      q.put(message_size + message)
+                     
+    
 print("Server socket")
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('192.168.43.50', 6565))
-
+serversocket.bind(('192.168.43.50', 6333ss))
 print("socket for accepting the connection",serversocket)
 serversocket.listen(10)
-(clientsocket, address) = serversocket.accept()
+
+while True:
+	sock, add = serversocket.accept()
+	q = queue.Queue()
+	with q_list_lock:
+		q_list.append(q)
+
+	tr = threading.Thread(target = rec_from_client, name = "receiving thread", args = [q, sock], daemon = True)
+	ts = threading.Thread(target = send_to_client, name = "sending thread", args = [q, sock], daemon = True)	
+	tr.start()
+	ts.start()

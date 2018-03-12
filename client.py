635812e5c2e8932055e7424e7798be580724427c
Neha Nagpal <nehanagpal.nn34@gmail.com>
2018-03-12 13:53:39 +0530

    commit..

--- client.py
+++ client.py
@@ -14,17 +14,24 @@ def length_to_seven(size):
 
 #receive messages from server
 def rec_msg_fromserver(server_sock):
-    msg_size= int(rec_whole_size(server_sock, 3).decode("utf-8"))
-    message = rec_whole_size(server_sock,msg_size).decode("utf-8")
-    print(message)    
-
+    msg_size= int(rec_whole_size(server_sock, 7).decode("utf-8"))
+    message = rec_whole_size(server_sock,msg_size)   
+    if 'file: '.encode('utf-8') in message:
+        p=message.strip('file: '.encode('utf-8'))
+        file_to_write = open('xyz.txt', 'wb')
+        file_to_write.write(p)
+        file_to_write.close()
+        print ('File received successfully')
+    else:
+        print(message)
+      
 
 print("Hi,I'm client")
 username= input("Username:")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("socket created")
 print("connecting to server.....")
-s.connect(('192.168.43.50',4901))
+s.connect(('192.168.43.50',48023))
 print("Now you are connected to server,Start the communication.")
 
 t = threading.Thread(target = rec_msg_fromserver, args=[s])
@@ -45,13 +52,11 @@ while True :
           size = length_to_seven(str(len(ap))).encode('utf-8')
           ap = size + ap
           s.sendall(ap)
-          file_to_send.close()
-          s.sendall(('File Sent by '+ username).encode('utf-8'))
-          s.close()
+          file_to_send.close()          
       
       else :
  
           message =username + ' sent : '+ msg
-          size = length_to_three(str(len(message)) )
+          size = length_to_seven(str(len(message)) )
           message = (size + message).encode("utf-8")
           s.sendall(message)
--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def send_to_client(my_q,client_sock):
     while True:
            
           message=my_q.get()
-         # print("sending to client")
+     
           client_sock.sendall(message)
           
 
@@ -26,41 +26,33 @@ def rec_from_client(my_q,client_sock):
     
        message_size = rec_whole_size(client_sock, 7)
        int_message_size = int(message_size.decode('utf-8'))
+       #p_msg_size=str(int_message_size - 6)
        message = rec_whole_size(client_sock, int_message_size)
-       
-       if 'file: '.encode('utf-8') in message:
+       #p=message.strip('file: '.encode('utf-8'))
+       while True:
+            if 'file: '.encode('utf-8') in message:
           
-         
-          with q_list_lock:
-              for q in q_list:
-                  if q is my_q:
-                      pass
-                  else :
-                      print('dfh')
-                      q.put(message_size + message)
-                      print(q.get())
-                      file_to_write = open('xyz.txt', 'wb')
-                      print('fdsg')
-                      file_to_write.write(q.get())
-                      print('fdsg111')
-                      file_to_write.close()
-                      print('fdsg222')
-                      print ('File received successfully')
-                      print('at')  
-       else:
+                with q_list_lock:
+                     for q in q_list:
+                         if q is my_q:
+                            pass
+                         else :
+                            q.put(message_size+ message)
+                     
+            else:
           
-          print("Client",message.decode('utf-8'))
-          with q_list_lock:
-              for q in q_list:
-                  if q is my_q:
-                      pass
-                  else :
-                      q.put(message_size + message)
+                print("Client",message.decode('utf-8'))
+                with q_list_lock:
+                     for q in q_list:
+                        if q is my_q:
+                           pass
+                        else :
+                             q.put(message_size + message)
                      
     
 print("Server socket")
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('192.168.43.50', 4901))
+serversocket.bind(('192.168.43.50', 48023))
 print("socket for accepting the connection",serversocket)
 serversocket.listen(10)
 
